70f9c8d7eb4f5ade7008b5b183a11253f764705d
Mahesh <mahesh51138@gmail.com>
2022-01-27 12:15:09 +0530

    added:leapyear

---
+++ src/com/bridgelabz/java/javacore.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.java;
+import java.util.Scanner;
+
+public class javacore {
+    private static Scanner sc;
+
+    public static void main(String[] args) {
+        int year;
+        sc = new Scanner(System.in);
+
+        System.out.println("\n Please Enter any year you wish: ");
+        year = sc.nextInt();
+
+        if (( year%400 == 0)|| (( year%4 == 0 ) &&( year%100 != 0))) {
+            System.out.format("\n %d is a Leap Year. \n", year);
+        }
+        else {
+            System.out.format("\n %d is NOT a Leap Year. \n", year);
+        }
+    }
+
+}
